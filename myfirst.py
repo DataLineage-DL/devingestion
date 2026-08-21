b2395f1e56a0feb797a5b8f309678c74a8e147ff
GANGIREDLASRIRAMAKOTI <gangiredlasriramakoti@gmail.com>
2026-08-21 17:06:50 +0530

    Update myfirst.py

--- myfirst.py
+++ myfirst.py
@@ -1,10 +1,15 @@
 def test():
     a = 10
     b=28
-    print(a+b) # data ingestion 
-
-
+    print(a+b) # data ingestion
+test()
 def multi():
     a=10
     b=20
     print(a*b)
+multi()
+def sub():
+    a=10
+    b=20
+    print(a-b)
+sub()
